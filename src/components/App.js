b22e9174e0b64693ea4aa7df47dce67b58bb203b
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2020-06-12 21:06:09 -0600

    Se incluyeron las paginas de sign in de admin y de usuarios

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ClientRouter from './ClientRouter';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { blue, purple } from "@material-ui/core/colors";
+import NavigationBar from './NavigationBar';
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: blue,
+    secondary: purple
+  }
+});
 
 export default function App() {
   return (
-    <ClientRouter />
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <NavigationBar />
+        <ClientRouter />
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
--- src/components/ClientRouter.js
+++ src/components/ClientRouter.js
@@ -1,10 +1,12 @@
 import React, { lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { HOME_PAGE, NOT_FOUND } from './../locations';
+import { HOME_PAGE, NOT_FOUND, ADMIN_SIGN_IN, SIGN_IN } from './../locations';
 import Loading from "./Loading";
 
 const HomePage = lazy(() => import("./HomePage"));
 const NotFoundPage = lazy(() => import("./NotFoundPage"));
+const AdminSignInPage = lazy(() => import("./AdminSignInPage"));
+const SignInPage = lazy(() => import("./SignInPage"));
 
 export default function ClientRouter() {
   return (
@@ -12,6 +14,8 @@ export default function ClientRouter() {
       <Switch>
         <Route exact path={HOME_PAGE()} component={HomePage} />
         <Route exact path={NOT_FOUND()} component={NotFoundPage} />
+        <Route exact path={ADMIN_SIGN_IN()} component={AdminSignInPage} />
+        <Route exact path={SIGN_IN()} component={SignInPage} />
         <Route path="*">
           <Redirect to="/404" />
         </Route>
