aa2cfa93a1120d54796482d6e2ffd7f3d12a36d2
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2020-06-18 19:16:33 -0600

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,2 @@
 export const API_URL = window.location.href.includes("vinil-oifa") ? "/" : "http://localhost:8080/";
-// export const SERVER_API_URL = "https://vinil-oifa.herokuapp.com/";
-export const SERVER_API_URL = "http://localhost:8080/";
+export const SERVER_API_URL = API_URL
